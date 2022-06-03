46ce539c135f560cf9275749d9cdfcd5594b016d
hyunju <dear.jjwim@gmail.com>
2022-06-03 22:57:38 +0900

    프로젝트 셋업, 깃허브 연동

---
+++ app/src/main/java/com/example/searchgithubapp/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.searchgithubapp
+
+import android.net.Uri
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.searchgithubapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+
+    private fun initView() = with(binding) {
+        loginButton.setOnClickListener {
+            loginGithub()
+        }
+    }
+
+    private fun loginGithub() {
+        val loginUri = Uri.Builder().scheme("https").authority("github.com")
+            .appendPath("login")
+            .appendPath("oauth")
+            .appendPath("authorize")
+            .appendQueryParameter("client_id", BuildConfig.GITHUB_CLIENT_ID)
+            .build()
+    }
+}
